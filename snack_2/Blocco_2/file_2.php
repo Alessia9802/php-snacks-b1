ec7624dd21378f11973beaacf15129c63a821c30
Alessia9802 <alessiamisciagna@yahoo.it>
2021-12-03 12:46:30 +0100

    Snack 2 blocco 2

---
+++ snack_2/Blocco_2/file_2.php
@@ -0,0 +1,18 @@
+<?php
+
+$invitati = ["Marco", "Luisa", "Elena", "Sofia", "Gianni", "Alberto"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
